54181721eef4fea8871edfda2b8b78bbd2ae25eb
cpl121 <cesar.peonlam@gmail.com>
2025-04-15 16:31:22 +0200

    feat: add lil-gui dependency

--- src/script.js
+++ src/script.js
@@ -1,6 +1,14 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import gsap from 'gsap'
+import GUI from 'lil-gui'
+
+/**
+ * Debug
+ */
+
+const gui = new GUI()
+const debugObject = {}
 
 /**
  * Base
@@ -14,10 +22,27 @@ const scene = new THREE.Scene()
 /**
  * Object
  */
+debugObject.color = '#3a6ea6'
 const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2)
-const material = new THREE.MeshBasicMaterial({ color: '#ff0000' })
+const material = new THREE.MeshBasicMaterial({ color: debugObject.color, wireframe: true })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+
+gui.add(mesh.position, 'y').min(-3).max(3).step(0.01).name('elevation')
+gui.add(mesh, 'visible')
+gui.add(material, 'wireframe')
+gui.addColor(debugObject, 'color').onChange(() => {
+    material.color.set(debugObject.color)
+})
+
+debugObject.spin = () => {
+    gsap.to(mesh.rotation, { y: mesh.rotation.y + Math.PI * 2 })
+}
+gui.add(debugObject, 'spin')
+
+/* gui.add(geometry, 'widthSegments').min(1).max(100).step(1).name('widthSegments') */
+debugObject.subdivision = 2
+gui.add(debugObject, 'subdivision').min(1).max(20).step(1).name('widthSegments')
 
 /**
  * Sizes
