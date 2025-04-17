092038a115ce53169124548ee72fe1980b361422
cpl121 <cesar.peonlam@gmail.com>
2025-04-17 14:42:05 +0200

    feat: lesson 10 - textures

--- src/script.js
+++ src/script.js
@@ -2,6 +2,67 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 /**
+ * Textures
+ */
+
+const loadingManager = new THREE.LoadingManager()
+
+const textureLoader = new THREE.TextureLoader(loadingManager)
+// const colorTexture = textureLoader.load('/textures/checkerboard-1024x1024.png')
+// const colorTexture = textureLoader.load('/textures/checkerboard-8x8.png')
+const colorTexture = textureLoader.load('/textures/minecraft.png')
+// const colorTexture = textureLoader.load('/textures/door/color.jpg')
+const alphaTexture = textureLoader.load('/textures/door/alpha.jpg')
+const heightTexture = textureLoader.load('/textures/door/height.jpg')
+const normalTexture = textureLoader.load('/textures/door/normal.jpg')
+const ambientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
+const metalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
+const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
+
+
+// colorTexture.repeat.x = 2
+// colorTexture.repeat.y = 3
+// colorTexture.wrapS = THREE.MirroredRepeatWrapping
+// colorTexture.wrapT = THREE.MirroredRepeatWrapping
+
+// colorTexture.offset.x = 0.5
+// colorTexture.offset.y = 0.5
+
+// colorTexture.rotation = Math.PI / 4
+// colorTexture.center.x = 0.5
+// colorTexture.center.y = 0.5
+
+// const image = new Image()
+// const texture = new THREE.Texture(image)
+// texture.colorSpace = THREE.SRGBColorSpace
+// image.addEventListener('load', () => {
+//     texture.needsUpdate = true
+// })
+// image.src = '/textures/door/color.jpg'
+
+// image.onload = () => {
+//     const texture = new THREE.Texture(image)
+// }
+
+colorTexture.generatedMipmaps = false
+colorTexture.minFilter = THREE.NearestFilter // If we use NearestFilter, we need to deactivate the mipmaps to improve the performance 
+colorTexture.magFilter = THREE.NearestFilter
+
+// Width conceptos
+// jpg more compression, png more quality
+// Use webs like tinypng to compress the images
+
+// Sizes
+// always use power of 2 resolutions by the mipmapping
+
+// Data
+// png support transparency, jpg not
+// if we want to have only one texture, we can combine the alpha with the color
+
+
+colorTexture.colorSpace = THREE.SRGBColorSpace
+
+/**
  * Base
  */
 // Canvas
@@ -14,7 +75,9 @@ const scene = new THREE.Scene()
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+console.log(geometry.attributes);
+
+const material = new THREE.MeshBasicMaterial({ map: colorTexture })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
@@ -83,4 +146,4 @@ const tick = () =>
     window.requestAnimationFrame(tick)
 }
 
-tick()
+tick() 
