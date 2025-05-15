a77b9140170e5870a8acb855c9d5a9b6f8d2ac0b
cpl121.eth <100352899+cpl121@users.noreply.github.com>
2025-05-15 11:21:12 +0200

    feat: particles (#14)

--- src/script.js
+++ src/script.js
@@ -3,16 +3,6 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
 
 /**
- * Textures
- */
-const textureLoader = new THREE.TextureLoader()
-const bakedShadow = textureLoader.load('/textures/bakedShadow.jpg')
-const simpleShadow = textureLoader.load('/textures/simpleShadow.jpg')
-bakedShadow.colorSpace = THREE.SRGBColorSpace
-simpleShadow.colorSpace = THREE.SRGBColorSpace
-
-
-/**
  * Base
  */
 // Debug
@@ -25,113 +15,47 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
- * Lights
+ * Textures
  */
-// Ambient light
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.2)
-gui.add(ambientLight, 'intensity').min(0).max(3).step(0.001)
-scene.add(ambientLight)
-
-// Directional light
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5)
-directionalLight.position.set(2, 2, - 1)
-gui.add(directionalLight, 'intensity').min(0).max(3).step(0.001)
-gui.add(directionalLight.position, 'x').min(- 5).max(5).step(0.001)
-gui.add(directionalLight.position, 'y').min(- 5).max(5).step(0.001)
-gui.add(directionalLight.position, 'z').min(- 5).max(5).step(0.001)
-scene.add(directionalLight)
-
-directionalLight.castShadow = true
-directionalLight.shadow.mapSize.width = 1024
-directionalLight.shadow.mapSize.height = 1024
-directionalLight.shadow.camera.top = 2
-directionalLight.shadow.camera.right = 2
-directionalLight.shadow.camera.bottom = -2
-directionalLight.shadow.camera.left = -2
-directionalLight.shadow.camera.near = 1
-directionalLight.shadow.camera.far = 6
-// directionalLight.shadow.radius = 10 // Only for PCFShadowMap
-
-const directionalLightHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
-directionalLightHelper.visible = false
-scene.add(directionalLightHelper)
-
-// Spot Light
-const spotLight = new THREE.SpotLight(0xffffff, 2, 10, Math.PI * 0.3)
-spotLight.castShadow = true
-spotLight.position.set(0, 2, 2)
-scene.add(spotLight)
-scene.add(spotLight.target)
-
-spotLight.shadow.mapSize.width = 1024
-spotLight.shadow.mapSize.height = 1024
-spotLight.shadow.camera.fov = 30 // Not useful
-spotLight.shadow.camera.near = 1
-spotLight.shadow.camera.far = 6
-
-const spotLightCameraHelper = new THREE.CameraHelper(spotLight.shadow.camera)
-spotLightCameraHelper.visible = false
-scene.add(spotLightCameraHelper)
-
-// Point Light
-const pointLight = new THREE.PointLight(0xffffff, 2.7)
-pointLight.castShadow = true
-pointLight.position.set(- 1, 1, 0)
-scene.add(pointLight)
-
-pointLight.shadow.mapSize.width = 1024
-pointLight.shadow.mapSize.height = 1024
-pointLight.shadow.camera.near = 0.1
-pointLight.shadow.camera.far = 5
-
-const pointLightCameraHelper = new THREE.CameraHelper(pointLight.shadow.camera)
-pointLightCameraHelper.visible = false
-scene.add(pointLightCameraHelper)
-
+const textureLoader = new THREE.TextureLoader()
+const particleTexture = textureLoader.load('/textures/particles/2.png')
 
 /**
- * Materials
+ * Particles
  */
-const material = new THREE.MeshStandardMaterial()
-material.roughness = 0.7
-gui.add(material, 'metalness').min(0).max(1).step(0.001)
-gui.add(material, 'roughness').min(0).max(1).step(0.001)
+// const particleGeometry = new THREE.SphereGeometry(1, 32, 32)
+// const particlesMaterial = new THREE.PointsMaterial({ size: 0.02, sizeAttenuation: true })
+// const particleMesh = new THREE.Points(particleGeometry, particlesMaterial)
+// scene.add(particleMesh)
 
-/**
- * Objects
- */
-const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 32, 32),
-    material
-)
+const particleGeometry = new THREE.BufferGeometry()
+const count = 20000
+const positions = new Float32Array(count * 3)
+const colors = new Float32Array(count * 3)
 
-sphere.castShadow = true
+for (let i = 0; i < count * 3; i++) {
+    positions[i] = (Math.random() - 0.5) * 10
+    colors[i] = Math.random()
+}
+particleGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+particleGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
 
-const plane = new THREE.Mesh(
-    new THREE.PlaneGeometry(5, 5),
-    // new THREE.MeshBasicMaterial({
-    //     map: bakedShadow // With this solution, we cant move the object
-    // })
-    material
-)
-plane.rotation.x = - Math.PI * 0.5
-plane.position.y = - 0.5
-plane.receiveShadow = true
+const particlesMaterial = new THREE.PointsMaterial()
+particlesMaterial.size = 0.1
+particlesMaterial.sizeAttenuation = true
+particlesMaterial.color = new THREE.Color('#ff88cc')
+// particlesMaterial.map = particleTexture
+particlesMaterial.alphaMap = particleTexture
+particlesMaterial.transparent = true
+// particlesMaterial.alphaTest = 0.001
+// particlesMaterial.depthTest = false
+particlesMaterial.depthWrite = false
+particlesMaterial.blending = THREE.AdditiveBlending
+particlesMaterial.vertexColors = true
 
-scene.add(sphere, plane)
 
-const sphereShadow = new THREE.Mesh(
-    new THREE.PlaneGeometry(1.5, 1.5),
-    new THREE.MeshBasicMaterial({
-        color: 0x000000,
-        transparent: true,
-        alphaMap: simpleShadow,
-    })
-)
-sphereShadow.rotation.x = -Math.PI * 0.5
-sphereShadow.position.y = plane.position.y + 0.01
-scene.add(sphereShadow)
-
+const particles = new THREE.Points(particleGeometry, particlesMaterial)
+scene.add(particles)
 
 /**
  * Sizes
@@ -161,9 +85,7 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 1
-camera.position.y = 1
-camera.position.z = 2
+camera.position.z = 3
 scene.add(camera)
 
 // Controls
@@ -179,10 +101,6 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-renderer.shadowMap.enabled = false
-// renderer.shadowMap.enabled = true
-renderer.shadowMap.type = THREE.PCFSoftShadowMap
-
 /**
  * Animate
  */
@@ -192,16 +110,16 @@ const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
 
-    // Update the sphere
-    sphere.position.x = Math.cos(elapsedTime) * 1.5
-    sphere.position.z = Math.sin(elapsedTime) * 1.5
-    sphere.position.y = Math.abs(Math.sin(elapsedTime * 3))
-
-    // Update the shadow
-    sphereShadow.position.x = sphere.position.x
-    sphereShadow.position.z = sphere.position.z
-    sphereShadow.material.opacity = (1 - sphere.position.y) * 0.5
-
+    // Update particles
+    // particles.rotation.y = elapsedTime * 0.2
+    for (let i = 0; i < count; i++) {
+        const i3 = i * 3
+        const y = i3 + 1
+        const x = particleGeometry.attributes.position.array[i3]
+        particleGeometry.attributes.position.array[y] = Math.sin(elapsedTime + x)
+    }
+    particleGeometry.attributes.position.needsUpdate = true
+    
     // Update controls
     controls.update()
 
