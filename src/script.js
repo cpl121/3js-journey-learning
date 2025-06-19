aa819197b0d035b1aa15b6e46a871cd6043ddbbe
cpl121 <cesar.peonlam@gmail.com>
2025-06-19 17:26:15 +0200

    feat: creating my first shader

--- src/script.js
+++ src/script.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
+import testVertexShader from './shaders/test/vertex.glsl'
+import testFragmentShader from './shaders/test/fragment.glsl'
 
 /**
  * Base
@@ -18,6 +20,7 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const flagTexture = textureLoader.load('/textures/flag-french.jpg')
 
 /**
  * Test mesh
@@ -25,11 +28,64 @@ const textureLoader = new THREE.TextureLoader()
 // Geometry
 const geometry = new THREE.PlaneGeometry(1, 1, 32, 32)
 
+const count = geometry.attributes.position.count
+const randoms = new Float32Array(count)
+
+for (let i = 0; i < count; i++) {
+    randoms[i] = Math.random()
+}
+
+geometry.setAttribute('aRandom', new THREE.BufferAttribute(randoms, 1))
+
 // Material
-const material = new THREE.MeshBasicMaterial()
+// const material = new THREE.MeshBasicMaterial()
+// const material = new THREE.RawShaderMaterial({
+const material = new THREE.ShaderMaterial({
+    vertexShader: testVertexShader,
+    fragmentShader: testFragmentShader,
+    // wireframe: true
+    // side: THREE.DoubleSide
+    // transparent: true,
+    uniforms: {
+        uFrecuency: { value: new THREE.Vector2(10, 5) },
+        uTime: { value: 0 },
+        uColor: { value: new THREE.Color('orange') },
+        uTexture: { value: flagTexture }
+    }
+})
+
+gui.add(material.uniforms.uFrecuency.value, 'x').min(0).max(20).step(0.01).name('frecuecyX')
+gui.add(material.uniforms.uFrecuency.value, 'y').min(0).max(20).step(0.01).name('frecuecyY')
+gui.addColor(material.uniforms.uColor, 'value')
+
+// const vertexShader = `
+//   varying vec2 vUv;
+
+//   void main() {
+//     vUv = position.xy * 0.5 + 0.5;
+//     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+//   }
+// `;
+
+// const fragmentShader = `
+//   precision mediump float;
+//   varying vec2 vUv;
+
+//   void main() {
+//     vec3 color = vec3(vUv, 1.0);
+//     gl_FragColor = vec4(color, 1.0);
+//   }
+// `;
+
+// const material = new THREE.ShaderMaterial({
+//   vertexShader,
+//   fragmentShader
+// });
+
 
 // Mesh
 const mesh = new THREE.Mesh(geometry, material)
+mesh.scale.y = 2/3
 scene.add(mesh)
 
 /**
@@ -84,6 +140,9 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Update material
+    material.uniforms.uTime.value = elapsedTime
 
     // Update controls
     controls.update()
